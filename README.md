31a26c217a98b90702a920f150af2d2b6c04b35b
luisfer2818 <luisfernando1011@hotmail.com>
2024-04-15 22:09:26 -0300

    primeiro commit subindo o repositorio

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CourseAngularV17
+# CourseAngularV16
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.11.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ` <h1>Curso de Angular V16</h1>
+    <router-outlet></router-outlet>`,
 })
 export class AppComponent {
-  title = 'course-angular-v17';
+  title = 'course-angular-V16';
 }
